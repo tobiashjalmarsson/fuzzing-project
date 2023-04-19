f58eb523914674f5bbff8ea74d76951b2588e756
tobiashjalmarsson <tobhja@student.chalmers.se>
2023-04-19 09:15:52 +0200

    express app

---
+++ applications/express_app/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+const port = 5000
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(port, () =>
+    console.log(`Example app listening on port ${port}!`),
+);
